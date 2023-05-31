5df81f8afb6b1c0a761069880fb7d93de5de2af9
parsaklgr <parsak11@gmail.com>
2023-06-01 02:33:15 +0330

    Change home directory to /balancesheet/

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,8 +4,8 @@
   <header class="w-full flex justify-between items-center p-6 bg-slate-200">
     <h1 class="text-4xl text-violet-800">Balance Sheet</h1>
     <nav class="flex gap-2">
-      <router-link to="/">Home</router-link>
-      <router-link to="/About">About us</router-link>
+      <router-link to="/balancesheet">Home</router-link>
+      <router-link to="/balancesheet/About">About us</router-link>
     </nav>
   </header>
 </template>
